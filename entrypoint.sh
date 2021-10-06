f565641016c8f8e581b2d4259a6a463bd696700e
Premkumar <prem.kumar@razorpay.com>
2021-10-06 18:55:09 +0530

    [Security]: updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 echo $DEFECT_DOJO_BASE_URL
+echo "ENV"
+env
+echo "LS"
 ls
+echo "PWD"
 pwd
+echo "Running app"
 java -DDEFECT_DOJO_BASE_URL=$DEFECT_DOJO_BASE_URL -DUPLOAD_FILE_PATH=$UPLOAD_FILE_PATH -jar /security-github-action-1.0-SNAPSHOT.jar
