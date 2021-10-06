ca9ffeb2016edf5c91548bce2d40e83cfeee684a
Premkumar <prem.kumar@razorpay.com>
2021-10-06 19:17:10 +0530

    [Security]: updated env value

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
-echo $DEFECT_DOJO_BASE_URL
+echo "Print DoJo Base URL"
+echo ${DEFECT_DOJO_BASE_URL}
 echo "ENV"
 env
 echo "LS"
@@ -7,4 +8,4 @@ ls
 echo "PWD"
 pwd
 echo "Running app"
-java -DDEFECT_DOJO_BASE_URL=$DEFECT_DOJO_BASE_URL -DUPLOAD_FILE_PATH=$UPLOAD_FILE_PATH -jar /security-github-action-1.0-SNAPSHOT.jar
+java -DDEFECT_DOJO_BASE_URL=${DEFECT_DOJO_BASE_URL} -DUPLOAD_FILE_PATH=${UPLOAD_FILE_PATH} -jar /security-github-action-1.0-SNAPSHOT.jar
