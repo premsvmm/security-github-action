6abdbffa3047fb3cf4977a81d483d372fe499933
Premkumar <prem.kumar@razorpay.com>
2021-10-06 19:26:09 +0530

    [Security]: added jar and updated file path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,2 @@
 #!/bin/sh -l
-echo "Print DoJo Base URL"
-echo ${DEFECT_DOJO_BASE_URL}
-echo "ENV"
-env
-echo "LS"
-ls
-echo "PWD"
-pwd
-echo "Running app"
-java -DDEFECT_DOJO_BASE_URL=${DEFECT_DOJO_BASE_URL} -DUPLOAD_FILE_PATH=${UPLOAD_FILE_PATH} -jar /security-github-action-1.0-SNAPSHOT.jar
+java -DDEFECT_DOJO_BASE_URL=${DEFECT_DOJO_BASE_URL} -DUPLOAD_FILE_PATH=${GITHUB_WORKSPACE}/${UPLOAD_FILE_PATH} -jar /security-github-action-1.0-SNAPSHOT.jar
