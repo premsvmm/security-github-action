d5538c7769c79cf3cade3b76d60a4f055bb8e14b
Premkumar <prem.kumar@razorpay.com>
2021-10-07 17:32:34 +0530

    [Security]: print token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 echo "GITHUB TOKEN"
-echo ${INPUT_GITHUB-TOKEN}
-echo "GITHUB TOKEN"
-echo ${{ github.token }}
-echo "github-token"
 echo ${github-token}
-java -DDEFECT_DOJO_BASE_URL=${DEFECT_DOJO_BASE_URL} -DUPLOAD_FILE_PATH=${GITHUB_WORKSPACE}/${UPLOAD_FILE_PATH} -DPR_NUM=${PR_NUM} -DGITHUB_TOKEN=${INPUT_GITHUB-TOKEN} -jar /security-github-action-1.0-SNAPSHOT-jar-with-dependencies.jar
+echo "GITHUB TOKEN"
+echo ${GIT_TOKEN}
+java -DDEFECT_DOJO_BASE_URL=${DEFECT_DOJO_BASE_URL} -DUPLOAD_FILE_PATH=${GITHUB_WORKSPACE}/${UPLOAD_FILE_PATH} -DPR_NUM=${PR_NUM} -DGITHUB_TOKEN=${GIT_TOKEN} -jar /security-github-action-1.0-SNAPSHOT-jar-with-dependencies.jar
